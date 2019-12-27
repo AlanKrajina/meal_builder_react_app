98f8ecb59fde79535a77207f2c178726d2e4acb4
Alan Krajina <alankrajina88@gmail.com>
2019-12-27 16:20:16 +0100

    created array from object, created MealIngredient with array elements

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -3,11 +3,19 @@ import classes from './Meal.css'
 import MealIngredient from './MealIngredient/MealIngredient'
 
 const meal = (props) => {
+    // creating an array from object
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            // ...Array(3) -> array with 3 elements
+            return [...Array(props.ingredients[igKey])].map((_, i) =>{
+                return <MealIngredient key={igKey + i} type={igKey} />
+            } )
+        })
+
     return (
             <div className={classes.Burger}>
                 <MealIngredient type='bread-top'/>
-                <MealIngredient type='cheese'/>
-                <MealIngredient type='meat'/>
+                {transformedIngredients}
                 <MealIngredient type='bread-bottom'/>
             </div>
 
