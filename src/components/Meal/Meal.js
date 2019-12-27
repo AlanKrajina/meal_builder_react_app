87789d36142ec823287c8a7f116ec86df2a83250
Alan Krajina <alankrajina88@gmail.com>
2019-12-27 16:28:38 +0100

    reduce method to flatten 1 array with 4 inner ar

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -3,14 +3,17 @@ import classes from './Meal.css'
 import MealIngredient from './MealIngredient/MealIngredient'
 
 const meal = (props) => {
+    //dinamic output of ingredients from mealBuilder
     // creating an array from object
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             // ...Array(3) -> array with 3 elements
             return [...Array(props.ingredients[igKey])].map((_, i) =>{
                 return <MealIngredient key={igKey + i} type={igKey} />
             } )
-        })
+        }).reduce((arr, el) => {
+            return arr.concat(el) // reduce method to flatten 1 array with 4 inner arrays into only 1 array with OBJECTS
+        }, []);
 
     return (
             <div className={classes.Burger}>
@@ -21,5 +24,4 @@ const meal = (props) => {
 
     )
 }
-
 export default meal;
