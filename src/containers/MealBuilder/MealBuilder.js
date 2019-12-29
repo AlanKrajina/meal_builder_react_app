b6acc839423a758b847379a0b0ce5ccc2c8ec9b7
Alan Krajina <alankrajina88@gmail.com>
2019-12-29 18:24:09 +0100

    created addIngredientHandler

--- src/containers/MealBuilder/MealBuilder.js
+++ src/containers/MealBuilder/MealBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Auxiliary';
 import Meal from '../../components/Meal/Meal';
 import BuildControls from '../../components/Meal/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    chesee: 04,
+    meat: 1.3,
+    bacon: 0.7
+}
+
 class MealBuilder extends Component {
     state ={
         ingredients: {
@@ -10,15 +17,32 @@ class MealBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 4
     }
+
+    addIngredientHandler = type => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+    }
+
+    removeIngredientHandler = type => {
+
+    }
+
 render () {
-
-
     return (
         <Aux>
             <Meal ingredients={this.state.ingredients}/>
-            <BuildControls />
+            <BuildControls ingredientAdded={this.addIngredientHandler} />
         </Aux>
     )
 
