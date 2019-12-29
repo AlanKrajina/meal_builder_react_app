71dca740d8806eb1ad6e2fce1c99859ef9a33e13
Alan Krajina <alankrajina88@gmail.com>
2019-12-29 17:58:42 +0100

    maped through controls

--- src/components/Meal/BuildControls/BuildControls.js
+++ src/components/Meal/BuildControls/BuildControls.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import React from 'react'
+import classes from './BuildControls.css'
+import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
     { label: 'Salad', type: 'salad'},
@@ -8,14 +10,14 @@ const controls = [
     { label: 'Meat', type: 'meat'},
 ]
 
-const buildControls = (props) => {
+const buildControls = (props) => (
 
 
-    return (
-        <div>
-            
+        <div className={classes.BuildControls}>
+            {controls.map(ctrl => (
+                <BuildControl key={ctrl.label} label={ctrl.label} />
+            ))}
         </div>
 
-    )
-}
+)
 export default buildControls;
