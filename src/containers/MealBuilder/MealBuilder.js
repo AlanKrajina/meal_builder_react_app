be02ee087447981c38049c07a274a44b25698f34
Alan Krajina <alankrajina88@gmail.com>
2019-12-30 09:53:48 +0100

    meat update

--- src/containers/MealBuilder/MealBuilder.js
+++ src/containers/MealBuilder/MealBuilder.js
@@ -6,7 +6,7 @@ import BuildControls from '../../components/Meal/BuildControls/BuildControls';
 const INGREDIENT_PRICES = {
     salad: 0.5,
     chesee: 0.4,
-    meat: 1.3,
+    meat: 1.5,
     bacon: 0.7
 }
 
@@ -16,7 +16,7 @@ class MealBuilder extends Component {
             salad: 0,
             bacon: 0,
             cheese: 0,
-            meat: 0
+            meat: 1
         },
         totalPrice: 4
     }
